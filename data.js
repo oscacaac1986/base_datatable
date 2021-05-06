827876aa1751d939ed5123d9284c4ed289b33346
oscar Cano Acosta <oscaralfredoca@gmail.com>
2021-05-06 00:06:32 -0500

    ultimo

--- data.js
+++ data.js
@@ -1,33 +1,138 @@
 $(document).ready(function () {
-    $('#tabla_dependencias').DataTable(
+    
+    let table
+    table= $('#tabla_dependencias').DataTable(
         {
             "language": {
                 "emptyTable": "Sin Datos para Mostrar...",
                 "info": "Mostrando pagina _PAGE_ de _PAGES_",
-                "infoEmpty":      "Mostrando 0 d 0 de 0 entradas",
+                "infoEmpty": "Mostrando 0 d 0 de 0 entradas",
                 "loadingRecords": "Cargando...",
-                "search":         "Buscar:",
+                "search": "Buscar:",
                 "paginate": {
-                    "first":      "First",
-                    "last":       "Last",
-                    "next":       "Siguiente",
-                    "previous":   "Anterior"
+                    "first": "First",
+                    "last": "Last",
+                    "next": "Siguiente",
+                    "previous": "Anterior"
                 },
-                "lengthMenu":     "Montrar _MENU_ entradas",
-              },
+                "lengthMenu": "Montrar _MENU_ entradas",
 
-              "ajax":{
-                  "url":"http://localhost/php_POO/clases.php?accion=dependencias",
-                  "dataSrc":"",
-              },
 
-              "columns":[
-                  {"data":"iddependencia"},
-                  {"data":"dependencia"},
-                  {"defaultContent":'<a href="#" class="btn btn-sm btn-success"><i class="bi bi-pencil-fill"></i></a>'},
-                  {"defaultContent":'<a href="#" class="btn btn-sm btn-danger"><i class="bi bi-trash"></i></a>'}
-              ],
-        }
+            },
+
+            "ajax": {
+                "url": "http://localhost/modulo_clientes/dependencia/php/crea_obj_dep.php?accion=listar",
+                "dataSrc": "",
+            },
+
+            "lengthMenu": [[7, 14, 21, 28, -1], [7, 14, 21, 28, "Todos"]],
+            "iDisplayLength": 7,
+
+            "columns": [
+                { "data": "iddependencia" },
+                { "data": "dependencia" },
+                { "defaultContent": '<button class=" btn-edit btn  btn-success" id="editar" type="button" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="bi bi-pencil-fill"></i></button>' },
+                { "defaultContent": '<button class="btn-delete btn  btn-danger" id="eliminar" type="button" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="bi bi-trash"></i></button>' }
+            ],
+        },
+
+        btneditar("#tabla_dependencias tbody",table)
     );
-    
+
+    $('#btn-insert').click(function (e) {
+        e.preventDefault();
+        let data = $('#insertData').val();
+        if (data == null || data.length == 0 || /\W \s/.test(data)) {
+            Command: toastr["warning"]("Debe diligenciar el campo Nombre de forma adecuada no puede llevar caracteres Especiales o estar Vacio ", "Dependencia")
+
+            toastr.options = {
+                "closeButton": true,
+                "debug": false,
+                "newestOnTop": false,
+                "progressBar": true,
+                "positionClass": "toast-top-right",
+                "preventDuplicates": false,
+                "onclick": null,
+                "showDuration": "300",
+                "hideDuration": "300",
+                "timeOut": "7000",
+                "extendedTimeOut": "1000",
+                "showEasing": "swing",
+                "hideEasing": "linear",
+                "showMethod": "fadeIn",
+                "hideMethod": "fadeOut"
+            }
+        } else {
+            insertDependencia(data)
+        }
+    });
+
 });
+
+
+function btneditar(tbody,table) {
+    $(tbody).on("click","button.btn-edit", function () {
+        id = $(this).parents("tr").find("td").eq(0).html();
+        $.ajax({
+            type: "GET",
+            url: "http://localhost/modulo_clientes/dependencia/php/crea_obj_dep.php?accion=queryinv&id=" + id,
+            success: function (response) {
+              let data=response
+                console.log(data)
+            }
+        });
+    });
+}
+
+
+function insertDependencia(data) {
+    let dataform =data.toUpperCase();
+    $.ajax({
+        type: "GET",
+        url: "https://localhost/modulo_clientes/dependencia/php/crea_obj_dep.php?accion=insertar&nombre=" + dataform,
+        success: function (response) {
+            let resp = response
+            if (resp == 1) {
+                Command: toastr["success"]("Dependencia Ingresada Correctamente", "Dependencia");
+                toastr.options = {
+                    "closeButton": true,
+                    "debug": false,
+                    "newestOnTop": false,
+                    "progressBar": true,
+                    "positionClass": "toast-top-right",
+                    "preventDuplicates": false,
+                    "onclick": null,
+                    "showDuration": "300",
+                    "hideDuration": "300",
+                    "timeOut": "2000",
+                    "extendedTimeOut": "1000",
+                    "showEasing": "swing",
+                    "hideEasing": "linear",
+                    "showMethod": "fadeIn",
+                    "hideMethod": "fadeOut"
+                }
+                $("#insertData").val("");
+            } else {
+                Command: toastr["error"]("Verifique los datos enviados", "Dependencia")
+
+                toastr.options = {
+                    "closeButton": true,
+                    "debug": false,
+                    "newestOnTop": false,
+                    "progressBar": true,
+                    "positionClass": "toast-top-right",
+                    "preventDuplicates": false,
+                    "onclick": null,
+                    "showDuration": "300",
+                    "hideDuration": "300",
+                    "timeOut": "2000",
+                    "extendedTimeOut": "1000",
+                    "showEasing": "swing",
+                    "hideEasing": "linear",
+                    "showMethod": "fadeIn",
+                    "hideMethod": "fadeOut"
+                }
+            }
+        }
+    });
+}
--- index.php
+++ index.php
@@ -13,6 +13,11 @@
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.24/css/jquery.dataTables.css">
     <!-- Icons -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css">
+
+    <!-- materialize -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+    <!-- Compiled and minified CSS -->
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"> -->
 </head>
 
 <body>
@@ -25,24 +30,91 @@
         </div>
     </header>
     <main>
-        <div class="container-fluid p-2">
+        <div class="container-fluid">
             <div class="row">
                 <div class="col-2"></div>
-                <div class="col-10 align-items-center justify-content-center">
-                    <table class="table" id="tabla_dependencias">
-                        <thead class="table-success justify-content-center">
-                            <tr class="text-center">
-                                <td>Id</td>
-                                <td>Nombre</td>
-                                <td>Editar</td>
-                                <td>Eliminar</td>
-                            </tr>
-                        </thead>
-                        <tbody></tbody>
-                    </table>
+                <div class="col-10">
+                    <section>
+                        <div class="row p-2">
+
+                            <div class="col-2">
+                                <div>
+                                    <button type="button" class="btn btn-success text-center" style=" 
+                                                width: 50px;
+                                                height: 50px;
+                                                border-radius: 50%;
+                                                box-shadow: 3px 2.5px 1px lightgray;
+                                                " title="Crear una nueva Dependencia" data-bs-toggle="modal" data-bs-target="#InserDepModal"><i class="bi bi-plus" width="32" height="32" viewBox="0 0 20 20" style="
+                                                display: flex;
+                                                align-items: center;
+                                                justify-content: center;
+                                                font-size:2rem;
+        "></i></button>
+                                </div>
+                            </div>
+                            <div class="col-10"></div>
+                        </div>
+                    </section>
+                    <section>
+                        <table class="table" id="tabla_dependencias">
+                            <thead class="table-success justify-content-center">
+                                <tr class="text-center">
+                                    <td><strong>Id</strong></td>
+                                    <td><strong>Nombre</strong></td>
+                                    <td><strong>Editar</strong></td>
+                                    <td><strong>Eliminar</strong></td>
+                                </tr>
+                            </thead>
+                            <tbody></tbody>
+                        </table>
+                    </section>
+
+                    <!-- Modal -->
+                    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    ...
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary btn-sm" data-bs-dismiss="modal">Close</button>
+                                    <button type="button" class="btn btn-primary ">Save changes</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Modal Insert -->
+
+                    <div class="modal fade" id="InserDepModal" tabindex="-1" aria-labelledby="InserDepModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header bg-primary">
+                                    <h5 class="modal-title" id="InserDepModalLabel">Insertar Dependencia</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <form method="get">
+                                        <div class="form-group">
+                                            <label for="" class="">Nombre:</label>
+                                            <input type="text" name="" id="insertData" class="form-control" >
+                                        </div>
+                                    </form>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                                    <button type="button" class="btn btn-primary " id="btn-insert">Guardar</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
+
     </main>
     <footer></footer>
 
@@ -53,6 +125,9 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
     <!-- JS Datatables -->
     <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.js"></script>
+    <!-- Alert -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+
     <!-- Archivo JS -->
     <script src="data.js"></script>
 </body>
